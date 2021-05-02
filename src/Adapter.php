8b5267f029229927444392ba5f20c5290b7aeba2
Till Wehowski <github@webfan.de>
2021-05-02 10:52:23 +0200

    Update Adapter.php

--- src/Adapter.php
+++ src/Adapter.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Adapter\Gcp;
+namespace Fusio\Adapter\Webfantize;
 
 use Fusio\Engine\AdapterInterface;
 
