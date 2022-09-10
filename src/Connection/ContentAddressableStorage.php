7a05ef33a7f65645bfa5e159e11a7be0ad55599a
Till Wehowski <github@webfan.de>
2022-09-10 18:05:30 +0200

    Create ContentAddressableStorage.php

---
+++ src/Connection/ContentAddressableStorage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Fusio\Adapter\Webfantize\Connection;
+
+use Fusio\Engine\ConnectionInterface;
+use Fusio\Engine\Form\BuilderInterface;
+use Fusio\Engine\Form\ElementFactoryInterface;
+use Fusio\Engine\ParametersInterface;
+use Fusio\Engine\Model\Connection;
+use frdl\cta\Server;
+
+class ContentAddressableStorage implements ConnectionInterface
+{
+	
+    public function getName()
+    {
+        return 'ContentAddressableStorage';
+    }
+
+         
+
+    public function getConnection(ParametersInterface $configuration) 
+    {
+
+         $StorageServer = new Server([
+             'chunksize' => intval($configuration->get('cta.options.chunksize')),
+             Server::URIS_DIR => $configuration->get('cta.storagedir.uris'),
+             Server::CHUNKS_DIR => $configuration->get('cta.storagedir.chunks'),
+             Server::FILES_DIR => $configuration->get('cta.storagedir.files'),             
+         ], true);
+    
+		    return $StorageServer;
+    }
+
+    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory)
+    {
+          $builder->add($elementFactory->newInput('cta.options.chunksize',
+												  '80', 'text','Chunksize - IT IS HIGHLY RECOMMENDED THAT YOU SET THIS VALUE TO "80"!!!'));
+                               
+          $builder->add($elementFactory->newInput('cta.storagedir.uris',
+												  'UriStorage Directory', 'text','The directory for the UriStorage (will be created if not exists).'));
+                          
+          $builder->add($elementFactory->newInput('cta.storagedir.chunks',
+												  'ChunksStorage Directory', 'text','The directory for the ChunksStorage (will be created if not exists).'));
+                          
+          $builder->add($elementFactory->newInput('cta.storagedir.files',
+												  'FilesStorage Directory', 'text','The directory for the FilesStorage (will be created if not exists).'));
+		   
+    }
+}
