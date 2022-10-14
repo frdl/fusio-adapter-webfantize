880d6b64ee04803a67e2f488a7e2cdccd735a884
Till Wehowski <github@webfan.de>
2022-10-15 01:53:14 +0200

    Update TemporaryDirectory.php

--- src/Connection/TemporaryDirectory.php
+++ src/Connection/TemporaryDirectory.php
@@ -1,23 +1,4 @@
 <?php
-/*
- * Fusio
- * A web-application to create dynamically RESTful APIs
- *
- * Copyright (C) 2015-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 namespace Fusio\Adapter\Webfantize\Connection;
 
@@ -31,7 +12,7 @@ use Fusio\Engine\Model\Connection;
 class TemporaryDirectory implements ConnectionInterface
 {
 	
-    public function getName()
+    public function getName() :string
     {
         return 'TemporaryDirectory';
     }
@@ -41,7 +22,7 @@ class TemporaryDirectory implements ConnectionInterface
      * @param \Fusio\Engine\ParametersInterface $config
      * @return \Spatie\TemporaryDirectory\TemporaryDirectory
      */
-    public function getConnection(ParametersInterface $configuration) : TempDir
+    public function getConnection(ParametersInterface $configuration) : mixed
     {
         $location =  $configuration->get('tmp.dir.location');
         $name =  $configuration->get('tmp.dir.name');
@@ -60,7 +41,7 @@ class TemporaryDirectory implements ConnectionInterface
 		return $temporaryDirectory;
     }
 
-    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory)
+    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory) :void
     {
           $builder->add($elementFactory->newInput('tmp.dir.location',
 												  'Temp-Directory', 
